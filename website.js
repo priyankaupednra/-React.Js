5c37689bb7f870f950701e1a0a354464d7c8a7f9
priyankaupednra <120468745+priyankaupednra@users.noreply.github.com>
2023-03-02 18:00:29 +0530

    Add files via upload

---
+++ website.js
@@ -0,0 +1,9 @@
+
+let btn = document.querySelector("button") 
+btn.addEventListener("click",()=>{
+    let site = document.getElementById("sitename").value 
+    let url = document.getElementById("siteurl").value  
+    console.log(site); 
+    console.log(url);
+}) 
+ 
