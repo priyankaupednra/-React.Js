eeff5f82ada8b523947aefdd059502d3e135b761
priyankaupednra <120468745+priyankaupednra@users.noreply.github.com>
2023-03-05 12:31:21 +0530

    Add files via upload

---
+++ Youtube.jsx
@@ -0,0 +1,33 @@
+import  React from 'react'  
+import './youtube.css';
+const  Youtube = () => {
+    return(
+        <>  
+        <div className='backgroundColor'> 
+        <div className='img'>
+            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepnglogos.com%2Fpics%2Fyoutube-logo-png&psig=AOvVaw0qKJX5AGlv1pdy5VQdeYNS&ust=1678083689847000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCLCQnYOTxP0CFQAAAAAdAAAAABAE" alt="" />
+        </div> 
+        <div className='divBlock'>
+            <ul>
+                <li>
+                    <a href="">Home</a>
+                </li> 
+                <li>
+                    <a href="">Shorts</a>
+                </li> 
+                <li>
+                    <a href="">Subscriptions</a>
+                </li> 
+                <li>
+                    <a href="">Libary</a>
+                </li>
+            </ul> 
+            </div>
+        </div> 
+        </> 
+        
+        
+        
+    )
+} 
+export default Youtube
