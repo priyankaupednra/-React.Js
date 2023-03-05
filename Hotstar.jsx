fe797bf2f7a286e2e384c2e4857e7d625a3569e4
priyankaupednra <120468745+priyankaupednra@users.noreply.github.com>
2023-03-05 12:35:04 +0530

    Add files via upload

---
+++ Hotstar.jsx
@@ -0,0 +1,33 @@
+import  React from 'react'  
+import './Hotstar.css' ;
+const  Hotstar = () => {
+    return(
+        <>  
+        <div className='backgroundColor'> 
+        <div className='img'>
+            <img src="" alt="" />
+        </div> 
+        <div className='divBlock'>
+            <ul>
+                <li>
+                    <a href="">Home</a>
+                </li> 
+                <li>
+                    <a href="">Tv</a>
+                </li> 
+                <li>
+                    <a href="">Movies</a>
+                </li> 
+                <li>
+                    <a href="">Sports</a>
+                </li>
+            </ul> 
+            </div>
+        </div> 
+        </> 
+        
+        
+        
+    )
+} 
+export default Hotstar
