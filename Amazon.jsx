23b261f52c3ebbb069a2ae2228918266e4cbfa2a
priyankaupednra <120468745+priyankaupednra@users.noreply.github.com>
2023-03-05 12:28:53 +0530

    Add files via upload

---
+++ Amazon.jsx
@@ -0,0 +1,33 @@
+import  React from 'react'  
+import './amazon.css';
+const  Amazon = () => {
+    return(
+        <>  
+        <div className='backgroundColor'> 
+        <div className='img'>
+            <img src="https://media.gettyimages.com/id/151368933/photo/amazon-holds-news-conference.jpg?s=612x612&w=gi&k=20&c=suuyQnRJ9w1b7fkGVAxwl4hht5M8NXD9cE_kDNpIkrA=" alt="" />
+        </div> 
+        <div className='divBlock'>
+            <ul>
+                <li>
+                    <a href="">Home</a>
+                </li> 
+                <li>
+                    <a href="">Offers</a>
+                </li> 
+                <li>
+                    <a href="">Deals</a>
+                </li> 
+                <li>
+                    <a href="">Fashion</a>
+                </li>
+            </ul> 
+            </div>
+        </div> 
+        </> 
+        
+        
+        
+    )
+} 
+export default Amazon
