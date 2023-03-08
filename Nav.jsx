7cfcfded9f9ab7800b0568204e913fb23cdb0b43
priyankaupednra <120468745+priyankaupednra@users.noreply.github.com>
2023-03-08 22:08:01 +0530

    Add files via upload

---
+++ Nav.jsx
@@ -0,0 +1,14 @@
+import React from "react" 
+import {Component} from "react" 
+class Nav extends Component 
+{
+    render(){
+        <>
+        return( 
+            <h1>{this.props.data}</h1>
+
+        )
+        </>
+    }
+} 
+export default Nav
