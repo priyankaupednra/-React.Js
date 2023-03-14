f6465f20a8faa2e946e0808341f4722d405b35b4
priyankaupednra <120468745+priyankaupednra@users.noreply.github.com>
2023-03-14 08:39:28 +0530

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,35 @@
+let counterElement = document.getElementById("counterValue") 
+
+function onIncrement(){
+    let previousCounterValue = counterElement.textContent;  
+    let updatedCounterValue = parseInt(previousCounterValue)+1 
+    if(updatedCounterValue > 0 ){
+        counterElement.style.color = "green"
+    } 
+    else if(updatedCounterValue < 0){
+        counterElement.style.color = "red"
+    } 
+    else {
+        counterElement.style.color = "black"
+    } 
+    counterElement.textContent = updatedCounterValue 
+} 
+function onDecrement() {
+    let previousCounterValue = counterElement.textContent 
+    let updatedCounterValue = parseInt(previousCounterValue)-1 
+    if(updatedCounterValue >0){
+        counterElement.style.color = "green" 
+    } 
+    else if(updatedCounterValue < 0 ){
+        counterElement.style.color = "red"
+    } 
+    else{
+        counterElement.style.color = "black"
+    } 
+    counterElement.textContent = updatedCounterValue
+} 
+function onReset(){
+    let counter =  0 
+    counterElement.textContent = counter 
+    counterElement.style.color = "black"
+}
