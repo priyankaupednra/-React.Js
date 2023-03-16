3ab1ed2402571ebd6f24d6b0f7e5e9c83f9825f7
priyankaupednra <120468745+priyankaupednra@users.noreply.github.com>
2023-03-16 20:24:12 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,20 +1,30 @@
-import React,{Component} from "react"; 
-import JSON  from  './User.json' 
-import Users from './Users.jsx' 
-export default class App extends Component {
-
- constructor() {
-    super()  
-    this.state = {json:JSON}
- }  
- render(){
-    return(
-        <div>
-            <Users data = {this.state.json}/>
+import React, { Component } from 'react'  
+import './count.css'
+export default class App extends Component { 
+    constructor()
+    {
+        super() 
+        this.state = {count : 0} 
+    }
+    handleIncrement = ()=>{
+      this.setState({count:this.state.count+1})
+    } 
+    handleDecrement = ()=>{
+      this.setState({count:this.state.count-1})
+    } 
+    handleReset = ()=>{
+      this.setState({count:0})
+    }
+  render() {
+    return (
+      <div className='mainBlock'>
+        <h1>{this.state.count}</h1> 
+        <div className='btngroup'> 
+            <button className='a' onClick={this.handleIncrement}>+Increment</button> 
+            <button className='b' onClick={this.handleDecrement}>-Decrement</button> 
+            <button onClick={this.handleReset}>Reset</button>
         </div>
+      </div> 
     )
- }
-} 
-
-
-    
+  }
+}
