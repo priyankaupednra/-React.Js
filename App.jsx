c9135fb7e0526317f53f2c178234fdc04cbde08e
priyankaupednra <120468745+priyankaupednra@users.noreply.github.com>
2023-03-17 21:45:34 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,30 +1,17 @@
-import React, { Component } from 'react'  
-import './count.css'
-export default class App extends Component { 
-    constructor()
-    {
-        super() 
-        this.state = {count : 0} 
-    }
-    handleIncrement = ()=>{
-      this.setState({count:this.state.count+1})
-    } 
-    handleDecrement = ()=>{
-      this.setState({count:this.state.count-1})
-    } 
-    handleReset = ()=>{
-      this.setState({count:0})
-    }
-  render() {
-    return (
-      <div className='mainBlock'>
-        <h1>{this.state.count}</h1> 
-        <div className='btngroup'> 
-            <button className='a' onClick={this.handleIncrement}>+Increment</button> 
-            <button className='b' onClick={this.handleDecrement}>-Decrement</button> 
-            <button onClick={this.handleReset}>Reset</button>
-        </div>
-      </div> 
-    )
-  }
+import React  from 'react' 
+import { ToastContainer,toast } from 'react-toastify'; 
+import 'react-toastify/dist/ReactToastify.css';
+ const App = () => {
+  let handleSubmit = ()=>{
+    toast.warning("logged in successfuly")
+  
 }
+  return(
+  <div>
+    <ToastContainer/> 
+    <button onClick={handleSubmit}>Click For Message</button>
+    </div>
+)
+}
+
+export default App ;
