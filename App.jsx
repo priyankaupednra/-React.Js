d262a9d3e0aa5717a5aab9dd4b0d180f25e7e181
priyankaupednra <120468745+priyankaupednra@users.noreply.github.com>
2023-03-19 16:45:21 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,17 +1,9 @@
-import React  from 'react' 
-import { ToastContainer,toast } from 'react-toastify'; 
-import 'react-toastify/dist/ReactToastify.css';
- const App = () => {
-  let handleSubmit = ()=>{
-    toast.warning("logged in successfuly")
-  
-}
-  return(
-  <div>
-    <ToastContainer/> 
-    <button onClick={handleSubmit}>Click For Message</button>
-    </div>
-)
-}
+import React from 'react'
+import Ref from './Ref.jsx'
+export const App = () => {
+  return (
+    <Ref/>
+  )
+} 
+export default App
 
-export default App ;
